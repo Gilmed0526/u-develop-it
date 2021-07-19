752b6f9eedbf8afa881829f88c35b9eb1e68d5ec
Gilmed0526git push origin maingit push origin main$ git remote -vgit commit -m page <gilbertomedina0526@gmail.com>
2021-07-19 18:23:08 -0400

    Added server conction

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Coonect to database
+const db = mysql.createConnection({
+
+    host: 'localhost',
+    //Your MYSQL username,
+    user: 'root',
+    //Your MYSQL Password
+    password: 'IronFan1826@', 
+    database: 'election'
+},
+console.log('Connceted to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows)=> {
+    console.log(rows);
+});
 
 
 //default response for any other request (Not Found)
